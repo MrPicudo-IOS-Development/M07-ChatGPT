6defb936ea0167e370096bc79bf27523f9fa7a02
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-04-28 23:24:51 -0600

    Conexión básica a la API
    Usando una interfaz sencilla para un textfield, un botón y una etiqueta.

---
+++ ChatGPTConexion/ChatGPTConexionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTConexionApp.swift
+//  ChatGPTConexion
+//
+//  Created by Miguel Torres on 28/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTConexionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
